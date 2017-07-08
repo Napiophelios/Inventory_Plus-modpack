17c3ebc8b4bfa3295386200bbd94651b766001a0
Napiophelios <napiophelios@gmail.com>
2017-07-08 17:21:25 -0400

    get rid of refresh button in survival mode

--- Default_Inventory/sfinv/init.lua
+++ Default_Inventory/sfinv/init.lua
@@ -1,5 +1,6 @@
 dofile(minetest.get_modpath("sfinv") .. "/api.lua")
 
+
 -- Create the trash field
 local trash = minetest.create_detached_inventory("crafting_trash", {
 	allow_put = function(inv, listname, index, stack, player)
@@ -11,6 +12,27 @@ local trash = minetest.create_detached_inventory("crafting_trash", {
 	end,
 })
 trash:set_size("main", 1)
+
+if minetest.setting_getbool("enable_damage") then
+sfinv.register_page("sfinv:crafting", {
+	title = "Crafting",
+	get = function(self, player, context)
+		return sfinv.make_formspec(player, context, [[
+   listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]
+   image[5.75,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]
+   image[0.075,1.6;0.8,0.8;crafting_trash_icon.png]
+   list[current_player;main;0,4.7;8,1;]
+   list[current_player;craft;2.5,0.5;3,3;]
+   listring[]
+   list[detached:crafting_trash;main;0,1.5;1,1;]
+   list[current_player;craftpreview;7,1.5;1,1;]
+			listring[current_player;main]
+			..default.gui_bg ..default.gui_bg_img ..default.gui_slots
+			]], true)
+	end
+})
+
+else
 
 sfinv.register_page("sfinv:crafting", {
 	title = "Crafting",
@@ -31,3 +53,4 @@ sfinv.register_page("sfinv:crafting", {
 			]], true)
 	end
 })
+end
